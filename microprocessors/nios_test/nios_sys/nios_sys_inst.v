641f718d0e9bbe07f0cb75f4137b038e819ae33c
James <Jamesestarks87@gmail.com>
2020-03-09 16:33:23 -0500

    Seven Segment w/ NIOS
    Weird stuff is happening when writing 0, 2, 3, C, and D to PIO registers. sven seg decoder module has been verified to work in a hardware only project.

--- microprocessors/nios_test/nios_sys/nios_sys_inst.v
+++
@@ -1,6 +0,0 @@
-	nios_sys u0 (
-		.byte_pio_export (<connected-to-byte_pio_export>), // byte_pio.export
-		.clk_clk         (<connected-to-clk_clk>),         //      clk.clk
-		.reset_reset_n   (<connected-to-reset_reset_n>)    //    reset.reset_n
-	);
-
---
+++ microprocessors/seven_segment/software/counter/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "system.h"
+#include "altera_avalon_pio_regs.h"
+int main()
+{
+	unsigned char count = 0x0;
+	unsigned int delay;
+
+	while(1)
+	{
+		printf("%i\t", count);
+		IOWR_ALTERA_AVALON_PIO_DATA(PIO_BYTE_DISPLAY_BASE, count);
+		delay = 0;
+
+		while(delay < 24000000)
+		{
+			delay++;
+		}
+		delay = 0;
+
+
+		if(count == 15)
+		{
+			count = 0;
+		}
+
+		count++;
+	}
+	return 0;
+}
