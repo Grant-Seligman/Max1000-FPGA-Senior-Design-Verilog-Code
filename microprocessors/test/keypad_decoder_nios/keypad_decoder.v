c9f7737dffea09ed73ce2fb6401bd9f5f8b06859
James Starks <Jamesestarks87@gmail.com>
2020-03-27 13:29:27 -0500

    Update
    update

--- microprocessors/test/keypad_decoder_nios/keypad_decoder.v
+++ microprocessors/test/keypad_decoder_nios/keypad_decoder.v
@@ -1,71 +1,42 @@
-module keypad_decoder (rows, key, columns, clk, rst);
-    output reg [3:0] rows;
+/*module keypad_decoder(columns, key, rows, clk)
+    output reg [3:0] columns, key;
+
+    input wire [3:0] rows;
+    input wire clk;
+
+    reg [3:0] state;
+
+always@(posedge clk) begin
+    case(state)
+end
+
+endmodule
+
+function check_column;
+    input [3:0] column; begin
+        
+    end
+endfunction
+*/
+module keypad_decoder(out, key, in);
+    output reg out;
     output reg [3:0] key;
 
-    input wire [3:0] columns;
-    input wire clk, rst;
+    input wire [3:0] in;
 
-    reg [1:0] state;
-    reg [3:0] key_data [3:0][3:0];
+    parameter one = 1000;
+    parameter two = 0100;
+    parameter three = 0010;
+    parameter a = 0001;
 
-    initial begin
-		state = 4'b0;
-        key_data[0][0] = 4'h1;
-        key_data[0][1] = 4'h2;
-        key_data[0][2] = 4'h3;
-        key_data[0][3] = 4'hA;
-        key_data[1][0] = 4'h4;
-        key_data[1][1] = 4'h5;
-        key_data[1][2] = 4'h6;
-        key_data[1][3] = 4'hB;
-        key_data[2][0] = 4'h7;
-        key_data[2][1] = 4'h8;
-        key_data[2][2] = 4'h9;
-        key_data[2][3] = 4'hC;
-        key_data[3][0] = 4'hE;
-        key_data[3][1] = 4'h0;
-        key_data[3][2] = 4'hF;
-        key_data[3][3] = 4'hD;
+    always@(in) begin
+        case(in)
+            one: key = 0001;
+            two: key = 0010;
+            three: key = 0011;
+            a: key = 1010;
+        endcase
     end
 
-    integer i, j;
-
-    parameter one   = 4'b0001;
-    parameter two   = 4'b0010;
-    parameter three = 4'b0100;
-    parameter four  = 4'b1000;
-
-    always @(posedge clk) begin
-		key = (j <= 0) ? key_data[i][j] : 4'b0000;
-		state = state + 1;
-		
-		case(state)
-			2'b00: begin
-				rows = one;
-				i = 0;
-			end
-			2'b01: begin
-				rows = two;
-				i = 1;
-			end
-			2'b10: begin
-				rows = three;
-				i = 2;
-			end
-			2'b11: begin
-				rows = four;
-				i = 3;
-			end
-		endcase
-
-		case(columns)
-			one:	 j = 0;
-			two: 	 j = 1;
-			three:	 j = 2;
-			four: 	 j = 3;
-			default: j = -1;
-		endcase
-	
-	end
 
 endmodule
