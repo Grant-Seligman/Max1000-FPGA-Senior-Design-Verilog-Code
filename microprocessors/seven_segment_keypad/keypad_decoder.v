b99d7bf2d237fc7046f78f0d820f091018ab8043
Grant-Seligman <57456886+Grant-Seligman@users.noreply.github.com>
2020-04-04 14:43:52 -0500

    Revised the keypad decoder and seven seg

--- microprocessors/seven_segment_keypad/keypad_decoder.v
+++ microprocessors/seven_segment_keypad/keypad_decoder.v
@@ -25,28 +25,28 @@ module keypad_decoder(key, row, column, clk);
 	always @ (posedge clk) begin
 		case (row)
 			4'b0111: 
-				if 		(!column[0]) key = 4'b0001;		
-				else if (!column[1]) key = 4'b0010;
-				else if (!column[2]) key = 4'b0011;
-				else if (!column[3]) key = 4'b1010;
+				if 		(!column[0]) key = 4'b0001; //1	
+				else if (!column[1]) key = 4'b0010; //2
+				else if (!column[2]) key = 4'b0011; //3
+				else if (!column[3]) key = 4'b1010; //A 
 				else row = 4'b1011;
 			4'b1011: 
-				if      (!column[0]) key = 4'B0100;
-				else if (!column[1]) key = 4'b0101;
-				else if (!column[2]) key = 4'b0110;
-				else if (!column[3]) key = 4'b1011;
+				if      (!column[0]) key = 4'b0100; //4
+				else if (!column[1]) key = 4'b0101; //5
+				else if (!column[2]) key = 4'b0110; //6
+				else if (!column[3]) key = 4'b1011; //B
 				else row = 4'b1101;
 			4'b1101: 
-				if 		(!column[0]) key = 4'b011;
-				else if (!column[1]) key = 4'b1000;
-				else if (!column[2]) key = 4'b1001;
-				else if (!column[3]) key = 4'b1100;
+				if 		(!column[0]) key = 4'b0111;//7 -- changed 011 to 0111
+				else if (!column[1]) key = 4'b1000; //8
+				else if (!column[2]) key = 4'b1001; //9
+				else if (!column[3]) key = 4'b1100; //C
 				else row = 4'b1110;
 			4'b1110: 
-				if 		(!column[0]) key = 4'b1110;
-				else if (!column[1]) key = 4'b0000;
-				else if (!column[2]) key = 4'b1111;
-				else if (!column[3]) key = 4'b1101;
+				if 		(!column[0]) key = 4'b1110; // star * or E
+				else if (!column[1]) key = 4'b0000; // 0
+				else if (!column[2]) key = 4'b1111; // pound # or F
+				else if (!column[3]) key = 4'b1101; // D
 				else row = 4'b0111;
 		endcase
 	end
